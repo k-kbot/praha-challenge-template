c85b576dced9ad81a4196a2569165d08f5c31957
k-kbot <polykkpodh1243@gmail.com>
2022-03-19 19:32:44 +0900

    controllerの作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { SampleController } from './controller/sample/some-data.controller'
+import { TeamsController } from './teams/teams.controller';
 
 // memo: DIコンテナとしては使わないため、controllerの追加だけしてください
 @Module({
   imports: [],
-  controllers: [SampleController],
+  controllers: [SampleController, TeamsController],
   providers: [],
 })
 export class AppModule {}
